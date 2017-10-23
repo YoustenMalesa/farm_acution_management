7e7782c3129fd76c6cd5f019555087af25fe00ad
Yousten Malesa <yousten@bbd.co.za>
2017-10-23 15:32:48 +0200

    Initial commit

---
+++ app/src/main/java/za/co/blue/demoapp/SplashScreen.java
@@ -0,0 +1,27 @@
+package za.co.blue.demoapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+    private final int SPLASH_DURATION = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent loginActivity = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(loginActivity);
+
+                finish();
+            }
+        }, SPLASH_DURATION);
+    }
+}
